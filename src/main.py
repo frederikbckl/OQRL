cf4657b9d48ff0d6a0c37a3b67ffe96110b57f74
Frederik Bickel <bickel.frederik@gmail.com>
2024-12-29 11:57:30 +0100

    Imports from agent and dqn

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
 """Main python file for offline quantum reinforcement learning PoC."""
 
+# import numpy as np
+
+from agent import AgentFactory
+from dqn import DQN
+
+# from train import run_train
+
 
 def main() -> None:
     """Initialize the Main entrypoint of the program."""
     print("Hello World!")
     env_name = "CartPole-v1"  # Setting environment to CartPole v1
-    # agent_fac = AgentFactory(DQN)  # Using DQN as the agent model
-    # num_epochs = 1000
-    # seed = 0
+    agent_fac = AgentFactory(DQN)  # Using DQN as the agent model
+    num_epochs = 1000
+    seed = 0
     # run_train(agent_fac, env_name, num_epochs, seed)  # Start the training process
 
 
