9a37edeaaa4d80e9f7e12fdaa51ea23267634b8d
Frederik Bickel <bickel.frederik@gmail.com>
2025-01-31 20:45:03 +0100

    Removed agent initializion doubling in main()

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 """Main entrypoint for Offline Quantum Reinforcement Learning PoC."""
 
-from agent import DQNAgent
 from train import run_train
 
 # import gymnasium as gym
@@ -11,17 +10,6 @@ def main():
     env_name = "CartPole-v1"
     num_epochs = 5
     seed = 0
-
-    # Initialize the agent
-    agent = DQNAgent(
-        obs_dim=4,
-        act_dim=2,
-        learning_rate=0.001,
-        gamma=0.99,
-        replay_capacity=10000,
-        batch_size=64,
-        vqc_layers=2,
-    )
 
     # Start training
     run_train(env_name, num_epochs, seed)
