302a9d3ff66be2d1a1d464c401d457d318afb189
Frederik Bickel <bickel.frederik@gmail.com>
2025-05-29 17:56:02 +0200

    Adjusted Hyperparameters  (subset_fraction, num_generations, population_size)

--- src/optim.py
+++ src/optim.py
@@ -12,8 +12,8 @@ class GAOptimizer:
     def __init__(
         self,
         model,
-        population_size=40,
-        num_generations=15,
+        population_size=50,
+        num_generations=20,
         mutation_rate=0.1,
         crossover_rate=0.5,
         rng=None,
